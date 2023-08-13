0fc0d14211e5a5545809a334dd0631e145c9553d
Fugguri <fygguri@icloud.com>
2023-08-14 00:20:25 +0300

    first commit

--- game-logic.js
+++ game-logic.js
@@ -24,7 +24,7 @@ const findRoom = (room) => {
     const creator = room.userName
 
     const isExist = rooms.find((r) => r.roomId === roomId)
-    console.log(isExist)
+    console.log(isExist.creator)
     return { isExist: !!isExist, creator: isExist }
 }
 
