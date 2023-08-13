ae2916d74435c3d5fc88401d9432980cf7dbde7a
Fugguri <fygguri@icloud.com>
2023-08-13 23:43:10 +0300

    first commit

--- game-logic.js
+++ game-logic.js
@@ -132,7 +132,7 @@ function isGameExist(data) {
 
     const isExist = findRoom(data)
     // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
-    this.emit('isGameExist', { isExist: isExist, gameId: data.gameId, mySocketId: this.id });
+    this.emit('isGameExist', { isExist: isExist.isExist, creator: isExist.creator, gameId: data.gameId, mySocketId: this.id });
 
     // Join the Room and wait for the other player
     this.join(data.gameId)
